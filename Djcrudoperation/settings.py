c0b45319918f8d22b0d18861eae0c6cd294929fb
Biswabhusan Sahoo <161637161+Biswa-source45@users.noreply.github.com>
2025-05-12 19:25:30 +0530

    Update settings.py

--- Djcrudoperation/settings.py
+++ Djcrudoperation/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-04gqi&n=1b^+uhp$c-)(gsgnw2g(tq+52rg82c=w0_gz_r9s9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
